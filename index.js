ef81ffcec7f7a2e6314e77cbcb795e0539beebc6
nategarvison <ngarvison@gmail.com>
2017-08-25 16:12:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  object[key] = value
+  
+  return object
 }
