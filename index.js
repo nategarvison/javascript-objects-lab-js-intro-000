4c7f88a5b83d6f923c985fca86d009066af141da
nategarvison <ngarvison@gmail.com>
2017-08-25 16:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  return var object = Object.assign({}, object, { [key]: value })
 }
+
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
